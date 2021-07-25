5efbf31067c5bf277c3fccf8254327570d501479
choisoonsin <cyh0214@gmail.com>
2021-07-26 00:38:49 +0900

    Added pourInCup method

--- src/main/java/com/example/template/CaffeineBeverage.java
+++ src/main/java/com/example/template/CaffeineBeverage.java
@@ -5,18 +5,24 @@ public abstract class CaffeineBeverage {
 	public final void prepareRecipe() {
 		boilWater();	// 카페인음료 공통 사항
 		brew();			// 끓이는 부분은 서브클래스에서 확장
+		pourInCup();
 
 		if(customerWantsCondiments()) {
 			addCondiments();
 		}
 	}
 	
+	
 	abstract void brew();
 	
 	abstract void addCondiments();
+
+	private void pourInCup() {
+		System.out.println("차를 붇는 중..");
+	}
 	
 	void boilWater() {
-		System.out.println("물 끓이는 중");
+		System.out.println("물 끓이는 중..");
 	}
 
 	// Hooking
